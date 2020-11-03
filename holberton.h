2bc01e092b313296bf9c4105e98f69f8ed8500da
karodri.p@gmail.com <karodri.p@gmail.com>
2020-11-03 02:46:49 +0000

    fixing betty in file

--- holberton.h
+++ holberton.h
@@ -5,18 +5,20 @@
 #include <stdio.h>
 
 int _printf(const char *format, ...);
-int ReturnNull();
+int ReturnNull(void);
 int CalculateMemoryAssignment(const char *format);
 int WriteToOutput(char *stringArray);
 void CopyArrayString(const char *stringArray, char *copiedString);
-void EvaluateFormat(const char *formatString,int index, int *count, va_list infiniteList);
+void EvaluateFormat(const char *formatString, int index, int *count,
+va_list infiniteList);
 void IntegerToString(int number, int *index);
 void UnsignedIntegerToString(unsigned int number, int *index);
 int GetDigit(int Base, int index, int count);
 void WriteString(char *stringArray, int *index);
 void WriteChar(char character, int *index);
 void WriteAddress(long address, int *index);
-void WriteHexadecimal(long address, int isUpperCase, int isMemoryAddress, int *index);
+void WriteHexadecimal(long address, int isUpperCase,
+int isMemoryAddress, int *index);
 void WriteOctal(long address, int *index);
 
 #endif
